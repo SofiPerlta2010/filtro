c418b7986a38c36d8a93b26dbf9cea844ec33aec
SofiPerlta2010 <93163172+SofiPerlta2010@users.noreply.github.com>
2023-05-24 18:13:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+    }
+    function setUp(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video
+    }
